c4e07d2a6d2ed089e90d50be20f51881754bfc14
Yael Serry <yaels@codevalue.net>
2018-01-10 11:20:17 +0200

    quizMonkey initialization

---
+++ app/home/home-view-model.js
@@ -0,0 +1,9 @@
+const Observable = require("data/observable").Observable;
+
+function HomeViewModel() {
+    const viewModel = new Observable();
+
+    return viewModel;
+}
+
+module.exports = HomeViewModel;
