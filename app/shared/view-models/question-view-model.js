c231721de65802f58cd815c84ceaa43532f3fda4
Yael Serry <yaels@codevalue.net>
2018-01-14 16:56:04 +0200

    answer choice in question now gives visual indication if right or wrong

--- app/shared/view-models/question-view-model.js
+++ app/shared/view-models/question-view-model.js
@@ -3,11 +3,40 @@ var observableModule = require("data/observable");
 function questionViewModel(question) {
     var viewModel = new observableModule.fromObject(question);
 
+viewModel.initQuestion = function () {
+    var newAnswersArray = [];
+    if (question.answers) {
+        for (i = 0; i < question.answers.length; i++) {
+            var answer = new observableModule.fromObject({
+                answerText: question.answers[i],
+                isSelected: false,
+                showCorrect: false,
+                isCorrect: i === question.correctAnswerIndex
+            });
+            newAnswersArray.push(answer);
+        }
+        viewModel.answers = newAnswersArray;
+    }
+}
+
+viewModel.checkMultipleChoiceAnswer = function (chosenAnswer) {
+    chosenAnswer.isSelected = true;
+    var answers = viewModel.get("answers");
+    var correctAnswer = answers[question.correctAnswerIndex];
+    correctAnswer.showCorrect = true;
+    return chosenAnswer.isCorrect;
+};
+
     return viewModel;
 }
 module.exports = questionViewModel;
 
-
+// if (answerIsCorrect) {
+//     console.log('correct answer with index ' + chosenAnswerIndex);
+// }
+// else {
+//     console.log('wrong answer with index ' + chosenAnswerIndex + ' while correctAnswerIndex was ' + correctAnswerIndex);
+// }
 
     // viewModel.initQuestion = function (question) {
     //     viewModel.set("questionText", question.questionText);
--- app/views/question-view/question-view.js
+++ app/views/question-view/question-view.js
@@ -26,7 +26,29 @@ exports.questionPageNavigatedTo = function (args) {
     questionIndex = context.currentQuestionIndex;
     quizLength = quiz.questions.length;
     vm = new QuestionViewModel(quiz.questions[questionIndex]);
+    vm.initQuestion();
     questionData.question = vm;
     questionData.progress = `${questionIndex + 1} / ${quizLength}`;
     setBackgroundColor(page);
+}
+
+// disableList = function (args) {
+//     var parent = args.object.parent;
+//     if (parent) {
+//         var list = view.getViewById(parent, "answersListView");
+//         if (list) {
+//             list.isUserInteractionEnabled = false;
+//         }
+//     }
+// }
+exports.onSelectMultipleChoiceAnswer = function (args) {
+    // disableList(args);
+    var chosenAnswer = args.view.bindingContext;
+
+    var answeredCorrectly = vm.checkMultipleChoiceAnswer(chosenAnswer);
+    console.log('answeredCorrectly: ' + answeredCorrectly);
+    // if (answeredCorrectly) {
+    //     quiz.incrementScore();
+    // }
+    // setTimeout(navigateToNextPage, 1000);
 }
