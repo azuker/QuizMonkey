6e39f0d4269d041e95ef46f7db39a746775b43d1
Yael Serry <yaels@codevalue.net>
2018-01-14 09:31:25 +0200

    fixed ip

--- app/shared/config.js
+++ app/shared/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     useLocalData: false,
-    apiUrl: "http://192.168.11.29:3001/" /* replace IP with your local IP  */
+    apiUrl: "http://192.168.10.125:3001/" /* replace IP with your local IP  */
 };
