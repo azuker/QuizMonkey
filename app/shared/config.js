408889eb3f85d12079e1d26328241590efb640f4
Yael Serry <yaels@codevalue.net>
2018-01-29 17:11:49 +0200

    added comment to config.js

--- app/shared/config.js
+++ app/shared/config.js
@@ -6,7 +6,7 @@ var serverIsRemote = true;
 var accessPath;
 
 if (serverIsRemote) {
-    accessPath = "192.168.10.85";
+    accessPath = "192.168.10.85"; /* paste remote server address here */
 }
 else if (platformModule.device.os === platformModule.platformNames.ios) {
     accessPath = "localhost";
